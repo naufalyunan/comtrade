6da1ef6c67278b1190d353fc633e8af8d66c990b
naufalyunan <naufalyunan45@gmail.com>
2022-03-25 00:38:58 +0700

    v2

--- route.js
+++ route.js
@@ -1,30 +1,26 @@
-const router = require('express').Router()
-const fs = require('fs')
-const axios = require('axios')
-const data = fs.readFileSync('./reporter2.json', 'utf-8')
-const reporter = JSON.parse(data).results
-console.log(reporter[0].id)
-router.get('/getData', (req, res, next) => {
-    let list = []
-    // (async function(arr) {
-        // for (let i = 0; i < reporter.length; i++) {
-        axios.get(`http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2013&r=all&p=0&rg=all&cc=AG2&fmt=json`)
-            .then( response => {
-                console.log(response.data)
-                list.push(response.data.dataset)
-                res.status(200)
-            })
-            .catch( err => {
-                res.json(err)
-            })
-        // }
+const router = require("express").Router();
+const fs = require("fs");
+const axios = require("axios");
+const data = fs.readFileSync("./reporter2.json", "utf-8");
+const reporter = JSON.parse(data).results;
+console.log(reporter[0].id);
+router.get("/getData", async (req, res, next) => {
+  let list = [];
+  // (async function(arr) {
+  for (let i = 0; i < reporter.length; i++) {
+    const { data } = await axios.get(
+      `http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2013&r=${reporter[i].id}&p=0&rg=all&cc=AG2&fmt=json`
+    );
     // }
-    
+    // }
+
     // )(list).then(v => {
     //     console.log(v)
     //     // console.log(list)
-    // })
-    // console.log(list)
-})
 
-module.exports = router
+    console.log(data);
+  }
+  console.log(fs);
+});
+
+module.exports = router;
