8caf91a24e63ad3c85b6f1b585797afae2688c11
naufalyunan <naufalyunan45@gmail.com>
2022-03-25 10:42:21 +0700

    fetch half

--- route.js
+++ route.js
@@ -1,26 +1,25 @@
 const router = require("express").Router();
 const fs = require("fs");
 const axios = require("axios");
-const data = fs.readFileSync("./reporter2.json", "utf-8");
+const data = fs.readFileSync("./reporter.json", "utf-8");
 const reporter = JSON.parse(data).results;
-console.log(reporter[0].id);
+
 router.get("/getData", async (req, res, next) => {
-  let list = [];
-  // (async function(arr) {
   for (let i = 0; i < reporter.length; i++) {
     const { data } = await axios.get(
       `http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2013&r=${reporter[i].id}&p=0&rg=all&cc=AG2&fmt=json`
     );
-    // }
-    // }
-
-    // )(list).then(v => {
-    //     console.log(v)
-    //     // console.log(list)
-
+    fs.writeFile(
+      `./result/${reporter[i].text}.json`,
+      JSON.stringify(data),
+      (err) => {
+        if (err) throw err;
+        console.log(`The ${reporter[i].text} file has been saved!`);
+      }
+    );
     console.log(data);
   }
-  console.log(fs);
+  res.status(200).send("finished");
 });
 
 module.exports = router;
