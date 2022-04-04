304d7e813318d6c1982f3ed76d233de02de3b952
naufalyunan <naufalyunan45@gmail.com>
2022-04-04 20:17:48 +0700

    2019

--- route.js
+++ route.js
@@ -7,10 +7,10 @@ const reporter = JSON.parse(data).results;
 router.get("/getData", async (req, res, next) => {
   for (let i = 0; i < reporter.length; i++) {
     const { data } = await axios.get(
-      `http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2020&r=${reporter[i].id}&p=0&rg=all&cc=AG2&fmt=json`
+      `http://comtrade.un.org/api/get?max=50000&type=C&freq=A&px=HS&ps=2019&r=${reporter[i].id}&p=0&rg=all&cc=AG2&fmt=json`
     );
     fs.writeFile(
-      `./result/2020/${reporter[i].text}.json`,
+      `./result/2019/${reporter[i].text}.json`,
       JSON.stringify(data),
       (err) => {
         if (err) throw err;
